d5f214f28a433018a3c60d2c7dd822a2f837aea1
quetapiton <oamost123@gmail.com>
2025-10-03 20:46:50 -0300

    Create cheat.js

---
+++ cheat.js
@@ -0,0 +1,10 @@
+// Você precisa digitar isso ao console, só copiar e colar e PRONTO!
+// esse é o jogo: https://thequetapitonstudio.github.io/FruitClicker/
+
+import("./scripts/lucky-block-system.js").then(hack1 => { window.hack = hack1 }) 
+  
+setInterval(() => { hack.buyLuckyBlock() }, 10)
+
+// se vc tiver mais ideias, me chame do Discord:
+
+// https://discord.com/invite/QgNDRBmb8g
