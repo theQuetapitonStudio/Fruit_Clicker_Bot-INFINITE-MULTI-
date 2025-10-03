8740d21bed49c18f96b099c76346f46b06a1541e
quetapiton <oamost123@gmail.com>
2025-10-03 20:37:33 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// se tiver mais ideias, chama mais no discord
+// https://discord.com/invite/QgNDRBmb8g
+
+import("./scripts/lucky-block-system.js").then(hack1 => { window.hack = hack1 }) 
+setInterval(() => { hack.buyLuckyBlock() }, 10)
+
+// jogo:
+
+// https://thequetapitonstudio.github.io/FruitClicker/
